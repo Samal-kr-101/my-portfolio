11ef22971f33906703a30b89b79df9063a1f6ad6
Samal-kr-101 <samalkr7739@gmail.com>
2025-09-02 20:15:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import Hero from './components/Hero/Hero'
 import About from './components/About/About'
 import Contact from "./components/Contact/Contact"
 import Footer from "./components/Footer/Footer"
-// import Education from "./components/Education/Education"
-// import Project from "./components/Project/Project"
+import Education from "./components/Education/Education"
+import Project from "./components/Project/Project"
 
 function App() {
   return (
